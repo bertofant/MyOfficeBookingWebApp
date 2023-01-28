b6a57359f33f9f282e32f4969d16f77dd48c3bdb
Roberto Fantini <roberto.fantini@telecomitalia.it>
2023-01-28 15:24:37 +0100

    Modificato Login e Registrazione per usare l'email come username

--- myauthenticator.py
+++ myauthenticator.py
@@ -8,25 +8,11 @@ class MyAuthenticate(Authenticate):
         super().__init__(credentials, cookie_name, key, cookie_expiry_days, preauthorized)
 
 
-    def find_email_in_credentials(self,email):
-        for user in self.credentials['usernames']:
-            if email == self.credentials['usernames'][user]['email']:
-                return True
-        return False
-
     def find_name_in_credentials(self,name):
         for user in self.credentials['usernames']:
             if name == self.credentials['usernames'][user]['name']:
                 return True
         return False
-
-    def find_name_from_email(self,email):
-        for user in self.credentials['usernames']:
-            if email == self.credentials['usernames'][user]['email']:
-                return user
-        return None
-
-
 
 
     def register_user(self, form_name: str, location: str='main', preauthorization=True) -> bool:
@@ -58,15 +44,15 @@ class MyAuthenticate(Authenticate):
             register_user_form = st.sidebar.form('Register user')
 
         register_user_form.subheader(form_name)
-        new_email = register_user_form.text_input('Email')
-        new_username = register_user_form.text_input('Username').lower()
+        new_email = register_user_form.text_input('Email').lower()
+        new_username = new_email
         new_name = register_user_form.text_input('Nome con cui verrà visualizzata la tua pianificazione')
         new_password = register_user_form.text_input('Password', type='password')
         new_password_repeat = register_user_form.text_input('Ripeti la password', type='password')
 
         if register_user_form.form_submit_button('Registrati'):
             if len(new_email) and len(new_username) and len(new_name) and len(new_password) > 0:
-                if new_username not in self.credentials['usernames'] and not self.find_email_in_credentials(new_email) and not self.find_name_in_credentials(new_name):
+                if new_username not in self.credentials['usernames'] and not self.find_name_in_credentials(new_name):
                     if new_password == new_password_repeat:
                         if preauthorization:
                             if new_email in self.preauthorized['emails']:
@@ -81,14 +67,12 @@ class MyAuthenticate(Authenticate):
                         raise RegisterError('Le Password inserite sono differenti')
                 else:
                     if new_username in self.credentials['usernames']:
-                        raise RegisterError('Username già esitente')
-                    elif self.find_email_in_credentials(new_email):
                         raise RegisterError('Email già utilizzata')
                     elif self.find_name_in_credentials(new_name):
                         raise RegisterError('Nome per la pianificazione già utilizzato')
 
             else:
-                raise RegisterError('Inserire email, username, nome e password')
+                raise RegisterError('Inserire email, nome e password')
 
     def _check_credentials(self, inplace: bool=True) -> bool:
         """
@@ -105,11 +89,6 @@ class MyAuthenticate(Authenticate):
         bool
             Validity of entered credentials.
         """
-        if self.username not in self.credentials['usernames']:
-            # check if the supplied username is actually an email
-            usernamefound = self.find_name_from_email(self.username)
-            if usernamefound != None:
-                self.username = usernamefound
 
         if self.username in self.credentials['usernames']:
             try:
@@ -135,3 +114,44 @@ class MyAuthenticate(Authenticate):
                 st.session_state['authentication_status'] = False
             else:
                 return False
+
+    def login(self, form_name: str, location: str='main') -> tuple:
+        """
+        Creates a login widget.
+
+        Parameters
+        ----------
+        form_name: str
+            The rendered name of the login form.
+        location: str
+            The location of the login form i.e. main or sidebar.
+        Returns
+        -------
+        str
+            Name of the authenticated user.
+        bool
+            The status of authentication, None: no credentials entered, 
+            False: incorrect credentials, True: correct credentials.
+        str
+            Username of the authenticated user.
+        """
+        if location not in ['main', 'sidebar']:
+            raise ValueError("Location must be one of 'main' or 'sidebar'")
+        if not st.session_state['authentication_status']:
+            self._check_cookie()
+            if st.session_state['authentication_status'] != True:
+                if location == 'main':
+                    login_form = st.form('Login')
+                elif location == 'sidebar':
+                    login_form = st.sidebar.form('Login')
+
+                login_form.subheader(form_name)
+                self.username = login_form.text_input('Email').lower()
+                st.session_state['username'] = self.username
+                self.password = login_form.text_input('Password', type='password')
+
+                if login_form.form_submit_button('Login'):
+                    self._check_credentials()
+
+        return st.session_state['name'], st.session_state['authentication_status'], st.session_state['username']
+
